f4a7dcfa4db1ddcaac4f640c4f63a313897fc7d4
caliny <oliveiracaliny@gmail.com>
2022-11-15 16:55:16 -0300

    Ajuste build para deploy

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import authMiddleware from './middlewares/authMiddleware';
+import PingController from './modules/ping/ping-controller';
 import AuthController from './modules/auth/auth-controller';
 import UserController from './modules/usuario/user-controller';
 
 const routes = express.Router();
-
+const pingController = new PingController();
 const authController = new AuthController();
 const usuarioController = new UserController();
 
+routes.get('/', pingController.ping);
 routes.post('/authenticate', authController.authenticate);
 
 // routes.get('/usuario', authMiddleware, usuarioController.find);
